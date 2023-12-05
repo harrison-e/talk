63fe77945ef88145aadb1a66179ba180c49d68fa
harry <harrisonceckert@icloud.com>
2023-12-04 21:04:25 -0500

    session

--- lib/include/Session.h
+++ lib/include/Session.h
@@ -9,12 +9,14 @@
 #include <future>
 
 #include "Friend.h"
+#include "Request.h"
 #include "Utility.h"
 
-using std::unique_ptr;
+using std::shared_ptr, std::make_shared;
 
-
-////// connection data
+/**************************
+ * CONSTS, STRUCTS, ETC.
+ **************************/
 const uint16_t PORT_RNG_ST = 33445;
 const uint16_t PORT_RNG_ND = 34445;
 
@@ -33,6 +35,15 @@ struct DHT_node bootstrap_nodes[] = {
          "B05C8869DBB4EDDD308F43C1A974A20A725A36EACCA123862FDE9945BF9D3E09"},
 };
 
+enum CONVO_TYPE {
+  CONVO_FRIEND,
+  CONVO_COUNT,
+  CONVO_NULL = UINT32_MAX,
+};
+
+const string FMT_FR = "\033[1;37;40m";
+const string FMT_ME = "\033[1;37;44m";
+const string FMT_ND = "\033[0m";
 
 /*
  * A Session encompasses the client's functionality
@@ -40,15 +51,18 @@ struct DHT_node bootstrap_nodes[] = {
 class Session {
 protected:  // data
   Tox* tox; // Tox instance
-
   // friend list (friend num is idx)
   // todo del_friend -> .at(n) = nullptr
-  vector<unique_ptr<Friend>> friend_list;
+  static vector<shared_ptr<Friend>> friends;
+  // request list
+  static vector<shared_ptr<Request>> requests;
+
+  Friend me; // todo
+  CONVO_TYPE conversation;
 
 protected: // methods
   ////// setup
   void setup();
-
   void init_tox();
   void bootstrap();
 
@@ -58,20 +72,21 @@ protected: // methods
   ////// commands
   void add_friend();
 
-  ////// callbacks
+  ////// callbacks for tox
   // me
+  static void self_conn_status_cb(Tox* tox, TOX_CONNECTION conn_status, void* user_data);
 
   // friend
-
-  ////// handlers
-  // me
-
-  // friend
-  static void handle_friend_req(const uint8_t *public_key, const uint8_t *message,
-                                size_t length, void *user_data);
-  static void handle_friend_msg(uint32_t friend_number, TOX_MESSAGE_TYPE type,
-                                const uint8_t *message, size_t length,
-                                void *user_data);
+  static void friend_req_cb(Tox* tox, const uint8_t* public_key, const uint8_t* message,
+                            size_t length, void* user_data);
+  static void friend_msg_cb(Tox* tox, uint32_t friend_number, TOX_MESSAGE_TYPE type,
+                            const uint8_t* message, size_t length, void* user_data);
+  static void friend_name_cb(Tox* tox, uint32_t friend_number, const uint8_t* name,
+                             size_t length, void* user_data);
+  static void friend_status_msg_cb(Tox* tox, uint32_t friend_number, const uint8_t* message,
+                                   size_t length, void* user_data);
+  static void friend_conn_status_cb(Tox* tox, uint32_t friend_number,
+                                    TOX_CONNECTION conn_status, void* user_data);
 
 public:
   ////// general
--- lib/src/Session.cpp
+++ lib/src/Session.cpp
@@ -9,6 +9,10 @@ void Session::setup() {
 
   // setup callbacks
   // todo
+  tox_callback_self_connection_status(tox, self_conn_status_cb);
+
+  tox_callback_friend_request(tox, friend_req_cb);
+  tox_callback_friend_message(tox, friend_msg_cb);
 }
 
 void Session::init_tox() {
@@ -35,13 +39,60 @@ void Session::bootstrap() {
   }
 }
 
-Session::Session() {
+Session::Session() : conversation {CONVO_NULL}
+{
   std::cout << "welcome to talk\n";
   setup();
   std::cout << "your public key:\n";
   print_pubkey();
-  // init rest...
+
+  // extra initialization
 }
+
+////// CALLBACKS
+void Session::self_conn_status_cb(Tox* tox, TOX_CONNECTION conn_status, void* user_data)
+{
+
+}
+
+void Session::friend_req_cb(Tox *tox, const uint8_t *public_key, const uint8_t *message,
+                            size_t length, void *user_data)
+{
+  requests.push_back(make_shared<Request>());
+  auto req = requests.back();
+  req->setPubKey(public_key);
+  req->setMessage((char *) message);
+}
+
+void Session::friend_msg_cb(Tox* tox, uint32_t friend_number, TOX_MESSAGE_TYPE type,
+                            const uint8_t *message, size_t length,
+                            void *user_data)
+{
+  auto f = friends.at(friend_number);
+  if (!f) return;
+  if (type == TOX_MESSAGE_TYPE_NORMAL) {
+    string s_msg = f->getName() + ": " + (char *) message;
+  }
+}
+
+void Session::friend_name_cb(Tox* tox, uint32_t friend_number, const uint8_t* name,
+                             size_t length, void* user_data)
+{
+
+}
+
+void Session::friend_status_msg_cb(Tox* tox, uint32_t friend_number, const uint8_t* message,
+                                   size_t length, void* user_data)
+{
+
+}
+
+void Session::friend_conn_status_cb(Tox* tox, uint32_t friend_number,
+                                    TOX_CONNECTION conn_status, void* user_data)
+{
+
+}
+
 
 ////// UTILITY
 void Session::print_pubkey() {
